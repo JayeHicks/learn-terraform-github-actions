805d3b7c158ab252702f32197ea327dd736eded0
Jaye Hicks <jaye.hicks@gmail.com>
2021-09-15 15:37:29 -0500

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "Jayes-Cool-Org"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo-github-actions"
     }
   }
 }
